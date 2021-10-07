5b8457776450f92f448d9b72203649a47cb6a600
charani013 <charanicherukuri013@gmail.com>
2021-10-07 06:55:34 +0000

    sol commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className="background-color">
+    <h1 className="heading-class">Super Over League</h1>
+    <div className="images-cards">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="images"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="images"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
